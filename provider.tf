0f450f056d0557deb8e235bf9e5a7189bae84a86
Siddu-28design <siddusteli08353@gmail.com>
2025-08-16 13:17:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -19,4 +19,8 @@ terraform {
 
 provider "azurerm" {
   features {}
+ client_id="d5d291cd-b6ff-4d21-8a52-9f1899042ac9"
+ client_secret="mR-8Q~5KrqDzGfEAu8noVPSkU~mg1wGIzNITibMs"
+ tenant_id="7e1b4521-93b6-473d-8e77-cf32c35bff05"
+ subscription_id="34a1c0d5-ab81-462c-89db-795e1572cd70"
 }
